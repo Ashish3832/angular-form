72015d7562c01e1a9604ca5906e9b3fbf1b74431
Ashish3832 <akumar6399@gmail.com>
2020-04-28 18:39:01 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent  {
   topics=['Angular','React','Vue'];
   topicHasError=true;
 
-  userModel=new User('ashish' , 'akumar6399@gmail.com' , 7351619673, 'default' ,'morning' , true);
+  userModel=new User('Roy' , 'roy1234@gmail.com' , 9897654352, 'default' ,'morning' , true);
 
 
   validateTopic(value)
